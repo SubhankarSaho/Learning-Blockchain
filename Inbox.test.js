aff63a182a2db1c635dd0f1a2fa4a8a9601d8e5a
SubhankarSaho <87765608+SubhankarSaho@users.noreply.github.com>
2022-12-31 11:57:01 +0530

    Update Inbox.test.js

--- Inbox.test.js
+++ Inbox.test.js
@@ -8,18 +8,31 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+let Initial_message = 'hi there!';
+let change_message = 'i am subhankar';
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ["hi there!"] })
+    inbox = await new web3.eth.Contract((interface))
+        .deploy({ data: bytecode, arguments: [Initial_message] })
         .send({ from: accounts[0], gas: '1000000' });
 
 });
 
 describe('deploys a contract', () => {
     it('deploy', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
     });
+    it('Has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, Initial_message);
+    });
+
+    it('it can change the message', async () => {
+        await inbox.methods.setMessage(change_message).send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, change_message);
+    }
+    )
 });
