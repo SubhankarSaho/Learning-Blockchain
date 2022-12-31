2d3fa165eb27fa383a867f66d1d6fcefe497e572
SubhankarSaho <87765608+SubhankarSaho@users.noreply.github.com>
2022-12-31 11:57:37 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -1,20 +1,8 @@
-// const path = require('path');
-// const solc = require('solc');
-// const fs = require('fs');
-
-// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-// const source = fs.readFileSync(inboxPath, 'UTF-8');
-
-// let comp = solc.compile(source, 1);
-// console.log(comp)
-
-
-
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxpath = path.resolve(__dirname, 'Contracts', 'Inbox.sol');
+const inboxpath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxpath, 'UTF-8');
 
 var input = {
@@ -34,12 +22,10 @@ var input = {
 };
 
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
-
-exports.interface = output.contracts['Inbox.sol']['Inbox'].interface;
-
-exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+var interface = output.contracts['Inbox.sol']['Inbox'].abi;
+var bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+console.log(interface);
 module.exports = {
     interface,
     bytecode,
 };
-
