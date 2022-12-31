4d6583ff863e182460029c2907ca84b5e87abc4f
SubhankarSaho <87765608+SubhankarSaho@users.noreply.github.com>
2022-12-31 11:56:13 +0530

    Update Inbox.sol

--- Inbox.sol
+++ Inbox.sol
@@ -1,23 +1,20 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 // pragma solidity >=0.4.24;
 
 contract Inbox {
     string public message;
 
-    constructor(string initialMessage) public {
+    constructor(string memory initialMessage) public {
         message = initialMessage;
     }
 
-    // function Inbox(string initialMessage) public{
-    //     message= initialMessage;
-    // }
-
-    function setMessege(string newMessage) public {
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
 
-    function getMessege() public view returns (string) {
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
